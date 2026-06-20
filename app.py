9ed2077c40eba9a4021b8f6db445c29f4d530984
silakozik <silakozik2005@gmail.com>
2026-06-20 13:50:01 +0300

    Add delete functionality for tasks and update index template with delete links

--- app.py
+++ app.py
@@ -20,17 +20,25 @@ def index():
     if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
-
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
             return 'There was an issue adding your task'
-
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
 
 if __name__ == '__main__':
     app.run(debug=True)
